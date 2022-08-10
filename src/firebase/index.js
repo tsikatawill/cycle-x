f0cc3aab3801c053373d5ee6e8b27f2f97fc0bf7
William M. Tsikata <tsikatawill@gmail.com>
2022-08-10 18:00:35 +0000

    initialize firebase

---
+++ src/firebase/index.js
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCPS2Ciy-hp5d3FuAKliMCRpN_SL0XdfQ8',
+  authDomain: 'cy-cle-x.firebaseapp.com',
+  projectId: 'cy-cle-x',
+  storageBucket: 'cy-cle-x.appspot.com',
+  messagingSenderId: '35786929775',
+  appId: '1:35786929775:web:59b8f94b2075911eda7f9a',
+}
+
+export const app = initializeApp(firebaseConfig)
