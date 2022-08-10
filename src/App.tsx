3d5dfaccdb5f74009f50615739198a5edf57cb9b
William M. Tsikata <tsikatawill@gmail.com>
2022-08-10 19:54:06 +0000

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 export default function App() {
-    return (
-        <div className="App">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis, asperiores? Quod
-            vero nam quidem quis corporis. Harum animi nemo maiores molestias soluta similique. Cum
-            labore sunt delectus, in rerum corrupti.
-        </div>
-    )
+  return (
+    <div className="App">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis,
+      asperiores? Quod vero nam quidem quis corporis. Harum animi nemo maiores
+      molestias soluta similique. Cum labore sunt delectus, in rerum corrupti.
+    </div>
+  )
 }
