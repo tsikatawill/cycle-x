71566612378c850e22404a2886aea2887c675b96
William M. Tsikata <tsikatawill@gmail.com>
2022-08-10 18:08:08 +0000

    initialize firebase

---
+++ src/firebase/index.js
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_APIKEY,
+    authDomain: process.env.REACT_APP_AUTHDOMAIN,
+    projectId: process.env.REACT_APP_PROJECTID,
+    storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+    messagingSenderId: process.env.REACT_APP_MSI,
+    appId: process.env.REACT_APP_APPID,
+    measurementId: process.env.REACT_APP_MEASUREID,
+}
+
+export const app = initializeApp(firebaseConfig)
