17b32864f0964794d5317d7532dee7ba79cefb5b
William M. Tsikata <tsikatawill@gmail.com>
2022-08-11 21:01:15 +0000

    import site sections

--- src/pages/homepage/Homepage.tsx
+++ src/pages/homepage/Homepage.tsx
@@ -1,43 +1,52 @@
+import AllProductsSection from '../../components/AllProductsSection'
 import Cycle from '../../images/cycle1.png'
 import Cycle2 from '../../images/cycle2.png'
 import Cycle3 from '../../images/cycle3.png'
-
+import FeaturedSection from '../../components/FeaturedSection'
+import LatestProductsSection from '../../components/LatestProductsSection'
 import {Slide} from '../../components/Slide'
 
 const Homepage = () => {
   return (
-    <header className="h-[35rem] sm:h-[30rem]">
-      <div className="container h-full py-5">
-        <Slide
-          slides={[
-            {
-              title: 'E-Mania 100m On a single charge',
-              img: Cycle,
-              description:
-                'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, ea exercitationem accusamus et minima minus adipisci repudiandae explicabo ipsum impedit nobis sequi autem nesciunt vitae tempora voluptate obcaecati dolor voluptatum!'
-            },
-            {
-              title: 'Mountain X-Trememo Super Sturdy',
-              img: Cycle2,
-              description:
-                'Cupiditate, ea exercitationem accusamus et minima minus adipisci repudiandae explicabo ipsum impedit nobis sequi autem nesciunt vitae tempora voluptate obcaecati dolor voluptatum!'
-            },
-            {
-              title: 'Kittie Choo Choo the playground all day',
-              img: Cycle3,
-              description:
-                'autem nesciunt vitae tempora voluptate obcaecati dolor voluptatum! Cupiditate, ea exercitationem accusamus et minima minus adipisci repudiandae explicabo ipsum impedit nobis sequi.'
-            },
-            {
-              title: 'Comfy Bum Bum',
-              img: Cycle3,
-              description:
-                'autem nesciunt vitae tempora voluptate obcaecati dolor voluptatum! Cupiditate, ea exercitationem accusamus et minima minus adipisci repudiandae explicabo ipsum impedit nobis sequi.'
-            }
-          ]}
-        />
-      </div>
-    </header>
+    <>
+      <header className="h-[35rem] sm:h-[30rem]">
+        <div className="container h-full py-5">
+          <Slide
+            slides={[
+              {
+                title: 'E-Mania 100m On a single charge',
+                img: Cycle,
+                description:
+                  'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, ea exercitationem accusamus et minima minus adipisci repudiandae explicabo ipsum impedit nobis sequi autem nesciunt vitae tempora voluptate obcaecati dolor voluptatum!'
+              },
+              {
+                title: 'Mountain X-Trememo Super Sturdy',
+                img: Cycle2,
+                description:
+                  'Cupiditate, ea exercitationem accusamus et minima minus adipisci repudiandae explicabo ipsum impedit nobis sequi autem nesciunt vitae tempora voluptate obcaecati dolor voluptatum!'
+              },
+              {
+                title: 'Kittie Choo Choo the playground all day',
+                img: Cycle3,
+                description:
+                  'autem nesciunt vitae tempora voluptate obcaecati dolor voluptatum! Cupiditate, ea exercitationem accusamus et minima minus adipisci repudiandae explicabo ipsum impedit nobis sequi.'
+              },
+              {
+                title: 'Comfy Bum Bum',
+                img: Cycle3,
+                description:
+                  'autem nesciunt vitae tempora voluptate obcaecati dolor voluptatum! Cupiditate, ea exercitationem accusamus et minima minus adipisci repudiandae explicabo ipsum impedit nobis sequi.'
+              }
+            ]}
+          />
+        </div>
+      </header>
+      <main>
+        <FeaturedSection />
+        <LatestProductsSection />
+        <AllProductsSection />
+      </main>
+    </>
   )
 }
 
