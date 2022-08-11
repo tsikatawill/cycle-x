c54303ee96e2c93fe444c4b5de7a072491783771
William M. Tsikata <tsikatawill@gmail.com>
2022-08-11 21:00:04 +0000

    create product card cpmponent

---
+++ src/components/ProductCard.tsx
@@ -0,0 +1,42 @@
+import {FC} from 'react'
+import {HiOutlineShoppingCart} from 'react-icons/hi'
+
+interface Props {
+  image: string
+  title: string
+  price: number
+  type: 'latest' | 'all'
+}
+
+const ProductCard: FC<Props> = ({image, title, price, type}) => {
+  return (
+    <div
+      className={`${
+        type === 'latest' ? 'w-64' : 'w-full'
+      } product-card bg-white  h-[23rem] flex-shrink-0`}
+    >
+      <div className="inner space-y-4 p-2 sm:p-5">
+        <div className="image-wrapper h-52 w-full">
+          <img
+            src={image}
+            alt={title}
+            className="h-full w-full object-cover mx-auto"
+          />
+        </div>
+
+        <div className="text-wrapper space-y-4">
+          <h3 className="title font-bold">{title}</h3>
+
+          <div className="flex justify-between items-center">
+            <p className="price">${price}</p>
+            <div className="add-to-cart h-8 w-8 grid place-content-center rounded-full cursor-pointer hover:bg-purple-300">
+              <HiOutlineShoppingCart size={20} className="cursor-pointer" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ProductCard
