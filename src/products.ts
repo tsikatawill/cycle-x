e61581240b2676a7d0b316bd8629871b79549795
William M. Tsikata <tsikatawill@gmail.com>
2022-08-16 18:53:57 +0000

    add product categories

--- src/products.ts
+++ src/products.ts
@@ -4,7 +4,7 @@ import Cycle2 from './images/cycle5.webp'
 import Part from './images/part4.webp'
 import Part2 from './images/part5.webp'
 
-const Products: CartItemI[] = [
+const AllProducts: CartItemI[] = [
   {
     image: Cycle,
     title: 'Awesome mountain bike available',
@@ -30,9 +30,40 @@ const Products: CartItemI[] = [
     image: Part,
     title: 'New brake shoes',
     price: 59,
+    type: 'all',
+    amount: 1
+  }
+]
+
+export const LatestProducts: CartItemI[] = [
+  {
+    image: Cycle,
+    title: 'Awesome mountain bike available',
+    price: 59,
+    type: 'latest',
+    amount: 1
+  },
+  {
+    image: Part2,
+    title: "Very 'sturdy' bike pedals",
+    price: 59,
+    type: 'latest',
+    amount: 1
+  },
+  {
+    image: Cycle2,
+    title: 'Great travelling/hiking companion',
+    price: 59,
+    type: 'latest',
+    amount: 1
+  },
+  {
+    image: Part,
+    title: 'New brake shoes',
+    price: 59,
     type: 'latest',
     amount: 1
   }
 ]
 
-export default Products
+export default AllProducts
